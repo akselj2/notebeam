72f15c647ed80b6b221d8a762db3cced159cd863
molotov <63641380+akselj2@users.noreply.github.com>
2024-03-19 08:32:59 +0100

    added Comments to Note.java

--- app/src/main/java/ch/zli/aj/notebeam/model/Note.java
+++ app/src/main/java/ch/zli/aj/notebeam/model/Note.java
@@ -3,6 +3,12 @@ package ch.zli.aj.notebeam.model;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+/**
+ * Note Model
+ * @author Aksel Jessen
+ * @version 1.0
+ * @since 18.03.2024
+ */
 public class Note {
 
     public UUID id;
@@ -11,6 +17,14 @@ public class Note {
     public String content;
     public Timestamp timestamp;
 
+    /**
+     * Note Constructor
+     * @param id UUID of Note
+     * @param title title of Note
+     * @param tag a tag for a Note (e.g. Work / Shopping)
+     * @param content content of Note
+     * @param timestamp time of creation / modification
+     */
     public Note(UUID id, String title, String tag, String content, Timestamp timestamp) {
         this.id = id;
         this.title = title;
